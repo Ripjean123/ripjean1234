580876732301342a27a66c941d3a4ffe3975cd0d
Ripjean123 <00001104124713sp@al.educacao.sp.gov.br>
2024-12-09 10:34:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'ciência e Geografia',
+    'Qual é o maior planeta do nosso sistema solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'Qual linguagem de programação é utilizada para desenvolver o modelo de linguagem do Meta AI, como o próprio modelo?',
+    'A linguagem de programação utilizada é Python, com frameworks como PyTorch e Transformers.'
+)
+
+criaCartao(
+    'ciência',
+    'Qual é o elemento químico com símbolo H?',
+    'H de Hidrogênio'
+)
